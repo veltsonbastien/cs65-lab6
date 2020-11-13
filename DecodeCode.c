4b8b0d29cc6ae393eeeb913b542ce21967215c48
veltsonbastien <bastienveltson@outlook.com>
2020-11-13 15:53:58 -0800

    thank you ari

--- DecodeCode.c
+++ DecodeCode.c
@@ -10,15 +10,14 @@ mipsinstruction decode(int value)
 	// TODO: fill in the fields
 	
 	//Check if it's signed or unsigned 
-	int posCheck = (value & (0x00008000))>>15 ; //will give the fifteenth position and determine if I orR 
+	int posCheck = (value & (0x00008000))>>15 ; //will give the fifteenth position and determine if I or R 
 
 //	printf("POS CHECK: %d\n", posCheck); 
 	
 	//It's an R-Type
 	if(posCheck == 1){
-	  instr.funct = value & (0x0000003F); 	   //should be the last 6 bits 
-//	  instr.immediate = ((value & (0x0000FFFF)) ^ 0x0000FFFF)+1   ; //should be the next 5 bits (treating it as shmt) 
-	  instr.immediate = -1; 
+	  instr.funct = value & (0x0000003F); 	   //should be the last 6 bits  
+	  instr.immediate = ((((value & (0x0000FFFF))^(0x0000FFFF))+1) << 31) >> 31; //get the 15 bits, do a two complement's to get the number, sll all the way to the left, and then all the way back to force it to make the number negative   
 	  instr.rd = value & (0x0000F800) >> 11; 	   //should be the next 5 bits 
 	  instr.rt = (value & (0x001F0000)) >> 16;         //should be the next 5 bits 
 	  instr.rs = (value & (0x03E00000)) >> 21;	   //should be the next 5 bits 
